c3a53e1430c41c636fc4d7a29e52258018e5d550
Rick Rosier <freekstyle_11@hotmail.com>
2015-05-19 11:24:50 +0200

    Added CardModel.cs

---
+++ CardsAgainstHumanity/CardsAgainstHumanity.WebApi/Migrations/CardModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CardsAgainstHumanity.WebApi.Migrations
+{
+    public class Card
+    {
+        public int ID { get; set; }
+        public int Black { get; set; }
+        public string Description { get; set; }
+    }
+}
