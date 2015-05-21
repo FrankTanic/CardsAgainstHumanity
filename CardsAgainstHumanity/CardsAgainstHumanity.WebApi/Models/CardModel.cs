0b9be264afe65647a2773624e9c98bab6c2c3d0d
Frank van Driel <frank.vandriel@hotmail.com>
2015-05-21 14:05:14 +0200

    Fixed the namespace of the Card model because the class was moved

--- CardsAgainstHumanity/CardsAgainstHumanity.WebApi/Models/CardModel.cs
+++ CardsAgainstHumanity/CardsAgainstHumanity.WebApi/Models/CardModel.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace CardsAgainstHumanity.WebApi.Migrations
+namespace CardsAgainstHumanity.WebApi.Models
 {
     public class Card
     {
