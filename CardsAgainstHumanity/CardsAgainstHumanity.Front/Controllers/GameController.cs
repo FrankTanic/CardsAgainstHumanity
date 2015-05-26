288e554bb67f52de407d7846a5a97097bc50f68b
Frank van Driel <frank.vandriel@hotmail.com>
2015-05-26 10:51:02 +0200

    Created a GameController with a Create Action and a Match Action with matching views

---
+++ CardsAgainstHumanity/CardsAgainstHumanity.Front/Controllers/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CardsAgainstHumanity.Front.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(int? id, string username)
+        {
+            if(id == null || username == null)
+            {
+                return View();
+            }
+
+
+
+            return RedirectToAction("Match");
+        }
+
+
+        public ActionResult Match()
+        {
+            return View();
+        }
+    }
+}
