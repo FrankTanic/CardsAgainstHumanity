8960e139eadc7adbe746768335a4c7819c65cd79
Frank van Driel <frank.vandriel@hotmail.com>
2015-05-31 23:05:43 +0200

    Created a GameController with a match view and layout view

--- CardsAgainstHumanity/CardsAgainstHumanity.Front/Controllers/GameController.cs
+++ CardsAgainstHumanity/CardsAgainstHumanity.Front/Controllers/GameController.cs
@@ -1,34 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using CardsAgainstHumanity.Front.Models;
 
 namespace CardsAgainstHumanity.Front.Controllers
 {
     public class GameController : Controller
     {
+        private HttpClient _httpClient;
+
+        public GameController()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:6408/"),
+            };
+        }
+
         public ActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Create(int? id, string username)
+        public ActionResult Create(int? id)
         {
-            if(id == null || username == null)
+            if(id == null)
             {
                 return View();
             }
 
-
-
-            return RedirectToAction("Match");
+            return RedirectToAction("Match", "Game", new { id = id });
         }
 
 
-        public ActionResult Match()
+        public async Task<ActionResult> Match(int? id)
         {
+            var response = await _httpClient.GetAsync("api/Game/GetGame/" + id + "");
+
+            if (response.IsSuccessStatusCode)
+            {
+                Game game = await response.Content.ReadAsAsync<Game>();
+
+                return View(game);
+            }
+
             return View();
         }
     }
