a96421adf0c9740b2790883eff6af6acc21fadc6
Frank van Driel <frank.vandriel@hotmail.com>
2015-05-31 23:28:42 +0200

    Created a game.js file for the signalr game mechanics

---
+++ CardsAgainstHumanity/CardsAgainstHumanity.Front/Content/js/game.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+
+    $.connection.hub.url = "http://localhost:6408/signalr";
+
+    // Create the connection with the hub
+    var game = $.connection.gameHub;
+
+    var username = prompt("Enter your username: ", "");
+
+    game.client.addChatMessage = function (message) {
+        $('#connected').append('<li>' + message + '</li>');
+    };
+
+    $.connection.hub.start(function () {
+        game.server.joinRoom(username, 1);
+    });
+
+    $.connection.hub.disconnected(function () {
+        game.server.leaveRoom(username, 1);
+    })
+
+});
