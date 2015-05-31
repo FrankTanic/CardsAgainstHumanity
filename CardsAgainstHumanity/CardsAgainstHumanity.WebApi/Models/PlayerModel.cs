ba31c72530c90ccf8ec41ea4bcb6ebba30334f10
Frank van Driel <frank.vandriel@hotmail.com>
2015-05-31 23:00:19 +0200

    Created a Game, connection and player model in the web api project

---
+++ CardsAgainstHumanity/CardsAgainstHumanity.WebApi/Models/PlayerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardsAgainstHumanity.WebApi.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public ICollection<Connection> Connections { get; set; }
+        public virtual ICollection<Game> Games { get; set; }
+    }
+}
