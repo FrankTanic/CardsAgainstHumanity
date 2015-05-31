90849eef9c3e7e0ae0478f206c5df38c7f6ab58f
Frank van Driel <frank.vandriel@hotmail.com>
2015-05-31 23:27:20 +0200

    Created a GameHub for the connection with the signalr client side

---
+++ CardsAgainstHumanity/CardsAgainstHumanity.WebApi/Hubs/GameHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using CardsAgainstHumanity.WebApi.Models;
+
+namespace CardsAgainstHumanity.WebApi.Hubs
+{
+    public class GameHub : Hub
+    {
+        private CardsAgainstHumanityDbContext _db = new CardsAgainstHumanityDbContext();
+
+        public async Task JoinRoom(string username, int roomID)
+        {
+            await Groups.Add(Context.ConnectionId, roomID.ToString());
+            Clients.Group(roomID.ToString()).addChatMessage(username + " joined.");
+        }
+
+        public Task LeaveRoom(string username, int roomID)
+        {
+            Clients.Group(roomID.ToString()).addChatMessage(username + "has lefted.");
+            return Groups.Remove(Context.ConnectionId, roomID.ToString());
+        }
+        
+    }
+}
