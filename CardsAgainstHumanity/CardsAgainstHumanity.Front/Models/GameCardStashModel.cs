e176ecdc4fd566ecf71d6c6efeb802364a4f859a
Frank van Driel <frank.vandriel@hotmail.com>
2015-06-09 13:14:38 +0200

    Created a new GameCardStashModel for saving data of the played cards

---
+++ CardsAgainstHumanity/CardsAgainstHumanity.Front/Models/GameCardStashModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardsAgainstHumanity.Front.Models
+{
+    public class GameCardStash
+    {
+        public int ID { get; set; }
+        public string ConnectionID { get; set; }
+        public Card Card { get; set; }
+        public Game Game { get; set; }
+    }
+}
