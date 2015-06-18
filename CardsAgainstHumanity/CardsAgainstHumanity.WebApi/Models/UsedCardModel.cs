0fabfbea64e88e75d214f8ace58d166d35218382
Frank van Driel <frank.vandriel@hotmail.com>
2015-06-18 20:36:55 +0200

    UsedCardModel in the Web api now has the username string property

--- CardsAgainstHumanity/CardsAgainstHumanity.WebApi/Models/UsedCardModel.cs
+++ CardsAgainstHumanity/CardsAgainstHumanity.WebApi/Models/UsedCardModel.cs
@@ -6,8 +6,9 @@ namespace CardsAgainstHumanity.WebApi.Models
     public class UsedCard
     {
         public int ID { get; set; }
-        public bool IsUsed { get; set; }
         public virtual Card Card { get; set; }
         public virtual Game Game { get; set; }
+        public string Username { get; set; }
+        public bool IsUsed { get; set; }
     }
 }
